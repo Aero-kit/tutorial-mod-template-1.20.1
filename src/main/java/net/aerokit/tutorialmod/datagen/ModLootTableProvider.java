4e6dd1f3a7ffc37aaed38b1b0bbb28a76b580131
Aero Kit <aerokittin@gmail.com>
2024-12-31 16:53:45 -0600

    Added Ruby: Stairs, Slab, Button, Pressure Plate, Fence, Fence Gate, Wall, Door, and TrapDoor.

--- src/main/java/net/aerokit/tutorialmod/datagen/ModLootTableProvider.java
+++ src/main/java/net/aerokit/tutorialmod/datagen/ModLootTableProvider.java
@@ -37,6 +37,19 @@ public class ModLootTableProvider extends FabricBlockLootTableProvider {
         addDrop(ModBlocks.DEEPSLATE_CITRINE_ORE, copperLikeOreDrops(ModBlocks.DEEPSLATE_CITRINE_ORE, ModItems.RAW_CITRINE));
         addDrop(ModBlocks.NETHER_CITRINE_ORE, copperLikeOreDrops(ModBlocks.NETHER_CITRINE_ORE, ModItems.RAW_CITRINE));
         addDrop(ModBlocks.END_STONE_CITRINE_ORE, copperLikeOreDrops(ModBlocks.END_STONE_CITRINE_ORE, ModItems.RAW_CITRINE));
+
+        addDrop(ModBlocks.RUBY_STAIRS);
+        addDrop(ModBlocks.RUBY_PRESSURE_PLATE);
+        addDrop(ModBlocks.RUBY_BUTTON);
+        addDrop(ModBlocks.RUBY_FENCE);
+        addDrop(ModBlocks.RUBY_FENCE_GATE);
+        addDrop(ModBlocks.RUBY_TRAPDOOR);
+        addDrop(ModBlocks.RUBY_WALL);
+
+        addDrop(ModBlocks.RUBY_DOOR, doorDrops(ModBlocks.RUBY_DOOR));
+        addDrop(ModBlocks.RUBY_SLAB, slabDrops(ModBlocks.RUBY_SLAB));
+
+
     }
     public LootTable.Builder copperLikeOreDrops(Block drop, Item item) {
         return dropsWithSilkTouch(drop, (LootPoolEntry.Builder)this.applyExplosionDecay(drop, ItemEntry.builder(item).apply(SetCountLootFunction.builder(UniformLootNumberProvider.create(2.0F, 5.0F))).apply(ApplyBonusLootFunction.oreDrops(Enchantments.FORTUNE))));
