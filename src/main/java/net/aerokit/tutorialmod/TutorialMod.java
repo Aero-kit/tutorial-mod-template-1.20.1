9aabf1e0f904d9a54e993c17984450be9d3e6f1b
Aero Kit <aerokittin@gmail.com>
2025-01-11 11:04:05 -0600

    Created custom recipes for Gem Polishing Block.

--- src/main/java/net/aerokit/tutorialmod/TutorialMod.java
+++ src/main/java/net/aerokit/tutorialmod/TutorialMod.java
@@ -6,6 +6,7 @@ import net.aerokit.tutorialmod.block.ModBlocks;
 import net.aerokit.tutorialmod.block.entity.ModBlockEntities;
 import net.aerokit.tutorialmod.entity.ModEntities;
 import net.aerokit.tutorialmod.entity.custom.PorcupineEntity;
+import net.aerokit.tutorialmod.recipe.ModRecipes;
 import net.aerokit.tutorialmod.screen.ModScreenHandlers;
 import net.aerokit.tutorialmod.sound.ModSounds;
 import net.aerokit.tutorialmod.util.ModCustomTrades;
@@ -39,6 +40,7 @@ public class TutorialMod implements ModInitializer {
 		ModBlockEntities.registerBlockEntities();
 
 		ModScreenHandlers.registerScreenHandlers();
+		ModRecipes.registerRecipes();
 
 		FuelRegistry.INSTANCE.add(ModItems.COAL_BRIQUETTE, 200);
 	}
